86d32f013c5bd2c38ca1507e8b2365ba56bb50ec
Amir Dhahri <amirdhahri01@gmail.com>
2024-09-24 13:46:27 +0100

    The first version of a simple signup form

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const container = document.querySelector(".container");
+const signupBtn = document.querySelector(".green-bg button");
+signupBtn.addEventListener("click" , ()=>{
+    container.classList.toggle("change");
+})
