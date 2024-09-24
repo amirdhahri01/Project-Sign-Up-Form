0156622b813603dd4858ab4e05baaf725db67691
Amir Dhahri <amirdhahri01@gmail.com>
2024-09-24 13:55:07 +0100

    A simple modification to the form

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const container = document.querySelector(".container");
 const signupBtn = document.querySelector(".green-bg button");
-signupBtn.addEventListener("click" , ()=>{
+signupBtn.addEventListener("click", () => {
     container.classList.toggle("change");
 })
